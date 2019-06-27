e38b0675d1e16d777c00980ef5948e04478c8e2c
hejnas <hejnas@gmail.com>
2019-06-27 13:25:34 +0200

    commit

---
+++ LH_SPI_MS5611.cpp
@@ -0,0 +1,8 @@
+/*
+ * 
+ * 		LH_SPI_MS5611.cpp
+ * 		author: Łukasz Hejna
+ * 		07.2019r.
+ *  
+ */
+
---
+++ LH_SPI_MS5611.h
@@ -0,0 +1,8 @@
+/*
+ * 
+ * 		LH_SPI_MS5611.h
+ * 		author: Łukasz Hejna
+ * 		07.2019r.
+ *  
+ */
+
